864240f6c2719bd1b86d89539d0b36da14315915
brittonm <68578865+brittonm@users.noreply.github.com>
2024-05-06 15:14:30 -0700

    Fixing initial release

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as f:
 
 setup(
     name="lasertools_pulsedispersiondata",
-    version="0.0.1.2",
+    version="0.0.1.3",
     description="A module to store data for laser pulse dispersion",
     license="GPLv3",
     long_description=long_description,
     author="brittonm",
     author_email="68578865+brittonm@users.noreply.github.com",
     url="https://github.com/lasertools/lasertools_pulsedispersiondata",
-    download_url = 'https://github.com/lasertools/lasertools_pulsedispersiondata/archive/refs/tags/v_0_0_1_1.tar.gz',
+    download_url = 'https://github.com/lasertools/lasertools_pulsedispersiondata/archive/refs/tags/v_0_0_1_3.tar.gz',
     packages=["lasertools_pulsedispersiondata"],
     install_requires=[
         "numpy",
